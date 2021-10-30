54b82fd943747e42a94d88086eabc18d2515f096
Pierre Dejoue <pierre.dejoue@gmail.com>
2021-10-30 19:20:28 +0200

    Modernize code

--- blueprints.py
+++ blueprints.py
@@ -12,6 +12,7 @@ import re
 import os
 import sys
 import zlib
+from collections.abc import Callable
 
 
 CONFIG_FILE = 'config.ini'
@@ -41,13 +42,11 @@ ENTITY_RENAMING_0_16_TO_0_17 = {
 }
 
 
-
-
-def full_db_path(db_path = DB_PATH):
+def full_db_path(db_path: str = DB_PATH) -> str:
     return os.path.join(SCRIPT_PATH, db_path)
 
 
-def create_db_directories(db_path = DB_PATH, not_a_book = NO_BOOK_NAME):
+def create_db_directories(db_path: str = DB_PATH, not_a_book: str = NO_BOOK_NAME) -> None:
     """create DB if not existing"""
     db_directory = full_db_path(db_path)
     if not os.path.exists(db_directory):
@@ -59,35 +58,33 @@ def create_db_directories(db_path = DB_PATH, not_a_book = NO_BOOK_NAME):
         os.makedirs(db_not_a_book_directory)
 
 
-def parse_blueprint_string(blueprint_base64):
-    assert isinstance(blueprint_base64, str)
+def parse_blueprint_string(blueprint_base64: str) -> str:
     version = int(blueprint_base64[0])
     assert version in BLUEPRINT_SUPPORTED_VERSIONS, 'Blueprint version number ' + str(version) + ' is not among the supported versions: ' + str(BLUEPRINT_SUPPORTED_VERSIONS)
     return zlib.decompress(base64.b64decode(blueprint_base64[1:])).decode()
 
 
-def generate_blueprint_string(blueprint_json):
-    assert isinstance(blueprint_json, str)
+def generate_blueprint_string(blueprint_json: str) -> str:
     return str(BLUEPRINT_VERSION) + base64.b64encode(zlib.compress(blueprint_json.encode())).decode()
 
 
 blueprint_filename_pattern = re.compile(r'^(([0-9]{3,}) - )?(.*)\.json$')
 
 
-def parse_blueprint_filename(filename):
+def parse_blueprint_filename(filename: str) -> dict:
     match = blueprint_filename_pattern.match(filename)
     if match:
         index = int(match.group(2)) if match.group(2) else -1
         return { 'index': index,  'name': match.group(3), 'filename': filename }
-    else:
-        return None
+    return None
 
-def generate_blueprint_filename(name, index = -1):
+
+def generate_blueprint_filename(name: str, index: int = -1) -> str:
     prefix = '{0:03d} - '.format(index) if index >= 0 else ''
     return prefix + name + '.json'
 
 
-def delete_book_content(book_directory):
+def delete_book_content(book_directory: str) -> None:
     for file in os.listdir(book_directory):
         file_path = os.path.join(book_directory, file)
         try:
@@ -99,7 +96,7 @@ def delete_book_content(book_directory):
             print('Error deleting blueprint file [' + file_path + ']: ' + str(err))
 
 
-def store_blueprint_book(book_obj, db_path = DB_PATH):
+def store_blueprint_book(book_obj: dict, db_path: str = DB_PATH) -> None:
     book_name = book_obj['blueprint_book']['label'] if 'label' in book_obj['blueprint_book'].keys() else 'no-name'
 
     # Create blueprint book folder, or erase its content if existing
@@ -113,7 +110,7 @@ def store_blueprint_book(book_obj, db_path = DB_PATH):
 
     # Create or update individual blueprints
     for blueprint_elt in book_obj['blueprint_book']['blueprints']:
-        blueprint_obj = { 'blueprint' : blueprint_elt['blueprint'] }
+        blueprint_obj = { 'blueprint': blueprint_elt['blueprint'] }
         blueprint_index = blueprint_elt['index']
         try:
             store_single_blueprint(blueprint_obj, blueprint_index, book_name, db_path)
@@ -124,7 +121,7 @@ def store_blueprint_book(book_obj, db_path = DB_PATH):
     store_book_version(book_obj['blueprint_book']['version'], book_name, db_path)
 
 
-def store_single_blueprint(blueprint_obj, blueprint_index = -1, book_name = NO_BOOK_NAME, db_path = DB_PATH):
+def store_single_blueprint(blueprint_obj: dict, blueprint_index: int = -1, book_name: str = NO_BOOK_NAME, db_path: str = DB_PATH) -> None:
     assert blueprint_index >= 0 or book_name == NO_BOOK_NAME, 'Cannot add an individual blueprint to a book yet'
     blueprint_name = blueprint_obj['blueprint']['label'] if 'label' in blueprint_obj['blueprint'].keys() else 'no-name'
     rel_db_path = os.path.join(book_name, generate_blueprint_filename(blueprint_name, blueprint_index))
@@ -133,34 +130,31 @@ def store_single_blueprint(blueprint_obj, blueprint_index = -1, book_name = NO_B
         print('Blueprint Updated: ' + rel_db_path)
     else:
         print('Blueprint Created: ' + rel_db_path)
-    fp = open(full_path, 'w')
-    json.dump(blueprint_obj, fp, sort_keys=True, indent=2, separators=(',', ': '))
-    fp.close()
+    with open(full_path, 'wt') as f:
+        json.dump(blueprint_obj, f, sort_keys=True, indent=2, separators=(',', ': '))
 
 
-def read_book_version(book_name, db_path = DB_PATH):
+def read_book_version(book_name: str, db_path: str = DB_PATH) -> int:
     assert book_name != NO_BOOK_NAME, 'Only valid blueprint books have a version file'
     rel_db_path = os.path.join(book_name, VERSION_FILE)
     full_path = os.path.join(full_db_path(db_path), rel_db_path)
-    fp = open(full_path, 'r')
-    version = int(fp.read().strip())
-    fp.close()
+    with open(full_path, 'rt') as f:
+        version = int(f.read().strip())
     return version
 
 
-def store_book_version(version, book_name, db_path = DB_PATH):
+def store_book_version(version: int, book_name: str, db_path: str = DB_PATH) -> None:
     assert book_name != NO_BOOK_NAME, 'Only valid blueprint books have a version file'
     rel_db_path = os.path.join(book_name, VERSION_FILE)
     full_path = os.path.join(full_db_path(db_path), rel_db_path)
-    fp = open(full_path, 'w')
-    fp.write(str(version) + '\n')
-    fp.close()
+    with open(full_path, 'wt') as f:
+        f.write(str(version) + '\n')
 
 
-def store_from_string(blueprint_raw_string, book_name = NO_BOOK_NAME, db_path = DB_PATH):
+def store_from_string(blueprint_raw: str, book_name: str = NO_BOOK_NAME, db_path: str = DB_PATH) -> int:
     result = 0
     try:
-        blueprint_json = parse_blueprint_string(blueprint_raw_string)
+        blueprint_json = parse_blueprint_string(blueprint_raw)
         blueprint_obj = json.loads(blueprint_json)
         if 'blueprint_book' in blueprint_obj.keys():
             assert book_name == NO_BOOK_NAME, 'Cannot store a blueprint book in another blueprint book'
@@ -182,12 +176,12 @@ def store_from_string(blueprint_raw_string, book_name = NO_BOOK_NAME, db_path = 
     return result
 
 
-def db_book_exists(book_name, db_path = DB_PATH):
+def db_book_exists(book_name: str, db_path: str = DB_PATH) -> bool:
     book_path = os.path.join(full_db_path(db_path), book_name)
     return os.path.exists(book_path)
 
 
-def get_book_contents(book_name, db_path = DB_PATH):
+def get_book_contents(book_name: str, db_path: str = DB_PATH) -> list:
     contents = []
     book_path = os.path.join(full_db_path(db_path), book_name)
     assert os.path.exists(book_path), 'Path does not exist [' + book_path + ']'
@@ -198,7 +192,7 @@ def get_book_contents(book_name, db_path = DB_PATH):
     return [a for a in contents if a is not None]
 
 
-def list_book(contents, book_name):
+def list_book(contents: list, book_name: str) -> None:
     if contents:
         if book_name == NO_BOOK_NAME:
             print('Individual Blueprints:')
@@ -208,7 +202,7 @@ def list_book(contents, book_name):
             print(' >> ' + bp_parsed_file['filename'])
 
 
-def list_db_all(db_path = DB_PATH):
+def list_db_all(db_path: str = DB_PATH) -> None:
     single_blueprints = []
     for file in os.listdir(full_db_path(db_path)):
         contents = []
@@ -221,51 +215,52 @@ def list_db_all(db_path = DB_PATH):
     list_book(single_blueprints, NO_BOOK_NAME)
 
 
-def list_db_book(book_name = NO_BOOK_NAME, db_path = DB_PATH):
+def list_db_book(book_name: str = NO_BOOK_NAME, db_path: str = DB_PATH) -> None:
     contents = get_book_contents(book_name, db_path)
     list_book(contents, book_name)
 
 
-def get_book_in_json(book_name, contents, version, active_index = 0):
+def get_book_in_json(book_name: str, contents: str, version: int, active_index: int = 0) -> str:
     assert contents, 'Empty book [' + book_name + ']'
     blueprints = [{'blueprint': bp_parsed_file['blueprint'], 'index': bp_parsed_file['index']} for bp_parsed_file in contents]
-    blueprint_book = { 'blueprint_book': {
-        'active_index': active_index,
-        'blueprints': blueprints,
-        'item': 'blueprint-book',
-        'label': book_name,
-        'version': version
-    }}
+    blueprint_book = {
+        'blueprint_book': {
+          'active_index': active_index,
+          'blueprints': blueprints,
+          'item': 'blueprint-book',
+          'label': book_name,
+          'version': version
+        }
+    }
     # Ensure the most compact JSON format
     json_string = json.dumps(blueprint_book, sort_keys=True, separators=(',', ':'))
     return json_string
 
 
-def get_db_book_in_json(book_name, db_path = DB_PATH):
+def get_db_book_in_json(book_name: str, db_path: str = DB_PATH) -> str:
     assert book_name != NO_BOOK_NAME, 'Can only decode a valid blueprint book'
     contents = get_book_contents(book_name, db_path)
     for bp_parsed_file in contents:
         rel_db_path = os.path.join(book_name, bp_parsed_file['filename'])
         full_path = os.path.join(full_db_path(db_path), rel_db_path)
-        fp = open(full_path, 'r')
-        bp_parsed_file['blueprint'] = json.load(fp)['blueprint']
-        fp.close()
+        with open(full_path, 'rt') as f:
+            bp_parsed_file['blueprint'] = json.load(f)['blueprint']
     version = read_book_version(book_name, db_path)
     return get_book_in_json(book_name, contents, version)
 
 
-def get_encoded_db_book(book_name, db_path = DB_PATH):
+def get_encoded_db_book(book_name: str, db_path: str = DB_PATH) -> str:
     return generate_blueprint_string(get_db_book_in_json(book_name, db_path))
 
 
-def process_blueprint_string(blueprint_string, stdout = False, book_name = NO_BOOK_NAME, db_path = DB_PATH):
+def process_blueprint_string(blueprint_string: str, stdout: bool = False, book_name: str = NO_BOOK_NAME, db_path: str = DB_PATH) -> None:
     if stdout:
         print(parse_blueprint_string(blueprint_string))
     else:
         store_from_string(blueprint_string, book_name, db_path)
 
 
-def match_filename(blueprint_info, blueprint_obj):
+def match_filename(blueprint_info: dict, blueprint_obj: dict) -> bool:
     assert blueprint_obj
     assert 'blueprint' in blueprint_obj
     modified = False
@@ -276,42 +271,47 @@ def match_filename(blueprint_info, blueprint_obj):
     return modified
 
 
-def update_entity_names(blueprint_info, blueprint_obj, entity_mapping):
+def update_entity_names(_blueprint_info: dict, blueprint_obj: dict, entity_mapping: dict) -> bool:
     assert blueprint_obj
     assert 'blueprint' in blueprint_obj
-    def f_str(dict, k):
-        assert k in dict
-        assert isinstance(dict[k], str) or isinstance(dict[k], unicode)
-        if dict[k] in entity_mapping:
-            dict[k] = entity_mapping[dict[k]]
+
+    def func_str(entity: dict, k: str):
+        assert k in entity
+        assert isinstance(entity[k], str)
+        if entity[k] in entity_mapping:
+            entity[k] = entity_mapping[entity[k]]
             return True
-        else:
-            return False
-    do_nothing = lambda dict, k: False
-    return walk_json_obj_and_map(blueprint_obj, f_str, do_nothing, do_nothing)
+        return False
+
+    def do_nothing(_entity: dict, _k: str): return False
+
+    return walk_json_obj_and_map(blueprint_obj, func_str, do_nothing, do_nothing)
 
 
-def walk_json_obj_and_map(json_obj, f_str, f_int, f_float):
+ProcessBlueprint = Callable[[dict, dict], bool]
+
+
+def walk_json_obj_and_map(json_obj, process_str: ProcessBlueprint, process_int: ProcessBlueprint, process_float: ProcessBlueprint) -> bool:
     modified = False
     if isinstance(json_obj, dict):
         for (key, value) in json_obj.items():
-            if isinstance(value, str) or isinstance(value, unicode):
-                modified = f_str(json_obj, key)
+            if isinstance(value, str):
+                modified = process_str(json_obj, key)
             elif isinstance(value, int):
-                modified = f_int(json_obj, key)
+                modified = process_int(json_obj, key)
             elif isinstance(value, float):
-                modified = f_float(json_obj, key)
+                modified = process_float(json_obj, key)
             else:
-                modified = modified | walk_json_obj_and_map(value, f_str, f_int, f_float)
+                modified = modified | walk_json_obj_and_map(value, process_str, process_int, process_float)
     elif isinstance(json_obj, list):
         for elt in json_obj:
-            modified = modified | walk_json_obj_and_map(elt, f_str, f_int, f_float)
+            modified = modified | walk_json_obj_and_map(elt, process_str, process_int, process_float)
     else:
         pass
     return modified
 
 
-def map_blueprint_json_files(files, f):
+def map_blueprint_json_files(files: list, process: ProcessBlueprint) -> None:
     for json_file in files:
         if not os.path.exists(json_file) or not os.path.isfile(json_file):
             print("Error: Skip [" + json_file + "]. File does not exist.")
@@ -320,20 +320,18 @@ def map_blueprint_json_files(files, f):
         if not blueprint_info:
             print("Error: Skip [" + json_file + "]. Expect JSON files with option --match-filename.")
             continue
-        fp = open(json_file, 'r')
-        blueprint_obj = json.load(fp)
-        fp.close()
+        with open(json_file, 'rt') as f:
+            blueprint_obj = json.load(f)
         if not blueprint_obj or 'blueprint' not in blueprint_obj:
             print("Error: Skip [" + json_file + "]. Wrong blueprint format.")
             continue
-        if f(blueprint_info, blueprint_obj):
+        if process(blueprint_info, blueprint_obj):
             print('Blueprint Updated: ' + json_file)
-            fp = open(json_file, 'w')
-            json.dump(blueprint_obj, fp, sort_keys=True, indent=2, separators=(',', ': '))
-            fp.close()
+            with open(json_file, 'wt') as f:
+                json.dump(blueprint_obj, f, sort_keys=True, indent=2, separators=(',', ': '))
 
 
-def map_blueprint_book(book_name, f, db_path = DB_PATH):
+def map_blueprint_book(book_name: str, process: ProcessBlueprint, db_path: str = DB_PATH) -> None:
     assert book_name != NO_BOOK_NAME
     contents = get_book_contents(book_name, db_path)
     files = []
@@ -341,7 +339,7 @@ def map_blueprint_book(book_name, f, db_path = DB_PATH):
         rel_db_path = os.path.join(book_name, bp_parsed_file['filename'])
         full_path = os.path.join(full_db_path(db_path), rel_db_path)
         files.append(full_path)
-    map_blueprint_json_files(files, f)
+    map_blueprint_json_files(files, process)
 
 
 
@@ -352,7 +350,7 @@ def main():
     parser.add_argument('files', metavar='FILE', nargs='*', help='Input files for options -f and -r')
     parser.add_argument('-s', '--from-strings', metavar='raw_string', dest='blueprint_strings', nargs='+', help='Store blueprints from raw strings')
     parser.add_argument('-f', '--from-files', dest='blueprint_files', action='store_true', help='Store blueprints from files, one raw string per line')
-    parser.add_argument('-b', '--book-name', metavar='book', dest='blueprint_book_name', default = NO_BOOK_NAME, help='Name of a blueprint book')
+    parser.add_argument('-b', '--book-name', metavar='book', dest='blueprint_book_name', default=NO_BOOK_NAME, help='Name of a blueprint book')
     parser.add_argument('-l', '--list', dest='list_db', action='store_true', help='List database content')
     parser.add_argument('-r', '--raw', dest='raw', action='store_true', help='Print out book or json file as a blueprint string')
     parser.add_argument('--json', dest='json', action='store_true', help='Print out the blueprints as JSON strings')
@@ -378,11 +376,11 @@ def main():
             map_blueprint_json_files(args.files, match_filename)
         elif args.update_to_0_17:
             assert args.blueprint_book_name != NO_BOOK_NAME or args.files, 'Must provide a book_name or JSON files with option --update-to-0.17'
-            f_update_to_0_17 = lambda info, obj: update_entity_names(info, obj, ENTITY_RENAMING_0_16_TO_0_17)
+            def func_update_to_0_17(info: dict, obj: dict): update_entity_names(info, obj, ENTITY_RENAMING_0_16_TO_0_17)
             if args.blueprint_book_name == NO_BOOK_NAME:
-                map_blueprint_json_files(args.files, f_update_to_0_17)
+                map_blueprint_json_files(args.files, func_update_to_0_17)
             else:
-                map_blueprint_book(args.blueprint_book_name, f_update_to_0_17)
+                map_blueprint_book(args.blueprint_book_name, func_update_to_0_17)
     else:
         # Load/store blueprints and blueprint books
         if args.blueprint_strings:
@@ -398,9 +396,9 @@ def main():
             for blueprint_file in args.files:
                 if not args.json:
                     print('Opening file: ' + blueprint_file)
-                fp = open(blueprint_file, 'r')
-                for blueprint_string in fp:
-                    process_blueprint_string(blueprint_string.strip(), args.json, args.blueprint_book_name)
+                with open(blueprint_file, 'rt') as f:
+                    for blueprint_string in f:
+                        process_blueprint_string(blueprint_string.strip(), args.json, args.blueprint_book_name)
         elif args.list_db:
             assert not args.raw, 'Incompatible options -l and -r'
             if args.blueprint_book_name == NO_BOOK_NAME:
@@ -411,12 +409,11 @@ def main():
             if args.blueprint_book_name == NO_BOOK_NAME:
                 assert args.files, 'No file or blueprint book specified with option -r'
                 for json_file in args.files:
-                    assert json_file[-5:] == '.json', 'Expect JSON files with option -r, but got [' +  json_file + ']'
-                    fp = open(json_file, 'r')
-                    # Ensure the most compact JSON format
-                    json_string = json.dumps(json.load(fp), sort_keys=True, separators=(',', ':'))
-                    print(generate_blueprint_string(json_string))
-                    fp.close()
+                    assert json_file[-5:] == '.json', 'Expect JSON files with option -r, but got [' + json_file + ']'
+                    with open(json_file, 'rt') as f:
+                        # Ensure the most compact JSON format
+                        json_string = json.dumps(json.load(f), sort_keys=True, separators=(',', ':'))
+                        print(generate_blueprint_string(json_string))
             else:
                 print(get_encoded_db_book(args.blueprint_book_name))
         elif args.json and args.blueprint_book_name != NO_BOOK_NAME:
